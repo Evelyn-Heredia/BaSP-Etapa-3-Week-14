c389d72bdfef43e657be633f241c9cae03aef869
Evelyn Heredia <evefhrd@gmail.com>
2022-06-21 00:37:30 -0300

    BaSP-W14: Locked out user test fixed

--- test/specs/locked.out.user.spec.js
+++ test/specs/locked.out.user.spec.js
@@ -1,10 +1,15 @@
-/*import LoginPage from '../pageobjects/login.page';
+import LoginPage from '../pageobjects/login.page';
 
 beforeAll('Open browser', ()=> {
     browser.url('https://www.saucedemo.com/');
 });
 
 describe('Locked out path testing', () => {
+    it('Empty password should display error', async () => {
+        await LoginPage.login('locked_out_user','' );
+        await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Password is required');
+    });
+
     it('Bot image should be displayed', async () => {
         await expect(LoginPage.robotImg).toBeDisplayed();
         browser.pause(3000);
@@ -15,19 +20,24 @@ describe('Locked out path testing', () => {
         browser.pause(5000);
     });
 
+    it('Page should be refreshed', async () => {
+        await browser.refresh();
+        await browser.pause(800);
+    });
+
     it('Locked out user should display error', async () => {
         await LoginPage.login('locked_out_user', 'secret_sauce');
         await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Sorry, this user has been locked out.');
     });
 
+    it('Page should be refreshed', async () => {
+        await browser.refresh();
+        await browser.pause(800);
+    });
+
     it('Empty username should display error', async () => {
         await LoginPage.login('', 'secret_sauce');
         await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required');
-    });
-
-    it('Empty password should display error', async () => {
-        await LoginPage.login('locked_out_user','' );
-        await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Password is required');
     });
 
     it('Invalid username should display error', async () => {
@@ -40,4 +50,3 @@ describe('Locked out path testing', () => {
         await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username and password do not match any user in this service');
     });
 });
-*/
